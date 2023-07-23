a653f56cbb636d691ce09ad9c2d9d1ad8cef82d0
Nissan1998 <jnnissanm@gmail.com>
2023-07-24 02:58:54 +0600

    admissionApi

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtwk5ft.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const admissionCollection = client
+      .db("admissionDB")
+      .collection("admission");
+
+    app.post("/admission", async (req, res) => {
+      const admitted = req.body;
+      const result = await admissionCollection.insertOne(admitted);
+      res.send(result);
+      console.log(admitted);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("collegeFinder is ongoing......");
+});
+
+app.listen(port, () => {
+  console.log(`collegeFinder is ongoing on port ${port}`);
+});
+
+// kAB4GxARUnXMAD5E
+// collegeFinder
